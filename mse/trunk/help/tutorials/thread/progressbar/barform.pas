16aa9d4b5822a19f2588f36e8224edfb3d8a8001
mse1 <mse1@adc01ae8-0b7f-0410-9d6f-9286f78004dd>
2007-12-27 07:16:45 +0000

    + Demo mse/trunk/help/tutorial/thread/progressbar.

---
+++ mse/trunk/help/tutorials/thread/progressbar/barform.pas
@@ -0,0 +1,63 @@
+unit barform;
+{$ifdef FPC}{$mode objfpc}{$h+}{$INTERFACES CORBA}{$endif}
+interface
+uses
+ msegui,mseclasses,mseforms,msegraphedits,mseevent,msegraphics,msegraphutils,
+ msemenus,msesimplewidgets,msewidgets,mseguithread;
+
+type
+ tbarfo = class(tmseform)
+   bar: tprogressbar;
+   tbutton1: tbutton;
+   thrTask: tthreadcomp;
+   procedure cancelex(const sender: TObject);
+   procedure taskexecute(const sender: tthreadcomp);
+   procedure taskfinished(const sender: tthreadcomp);
+ end;
+
+implementation
+
+uses
+ barform_mfm,main,sysutils;
+
+procedure tbarfo.taskexecute(const sender: tthreadcomp);
+var
+ i: integer;
+const
+ cnt = 20;
+begin
+ for i:= 1 to cnt  do begin
+  application.lock;
+  try
+   bar.value:= i/cnt;
+  finally
+   application.unlock;
+  end; 
+  sleep(200);
+  if sender.terminated then begin
+   break;
+  end;
+ end;
+end;
+
+procedure tbarfo.taskfinished(const sender: tthreadcomp);
+begin //application has already been locked by tthreadcomp
+ release;
+end;
+
+procedure tbarfo.cancelex(const sender: TObject);
+begin
+ application.lock;
+ try
+  if not releasing then begin //there could be a race condition 
+                              //with terminated thread
+   if askyesno('Do you wish to cancel the task?') then begin
+    thrTask.terminate;
+   end;
+  end;
+ finally
+  application.unlock;
+ end;
+end;
+
+end.
