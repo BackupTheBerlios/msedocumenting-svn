8d8c718a490603f8c4eb114105b0fe15a086f444
mse1 <mse1@adc01ae8-0b7f-0410-9d6f-9286f78004dd>
2010-10-26 09:19:15 +0000

    + Button demo.

---
+++ mse/trunk/help/tutorials/widgets/roundbutton/main.pas
@@ -0,0 +1,49 @@
+unit main;
+{$ifdef FPC}{$mode objfpc}{$h+}{$endif}
+interface
+uses
+ mseglob,mseguiglob,mseguiintf,mseapplication,msestat,msemenus,msegui,
+ msegraphics,msegraphutils,mseevent,mseclasses,mseforms,msesimplewidgets,
+ msewidgets,msebitmap,msedataedits,mseedit,msestrings,msetypes,msegraphedits,
+ msedispwidgets;
+
+type
+ tmainfo = class(tmainform)
+   trichbutton1: trichbutton;
+   timagelist1: timagelist;
+   tdatabutton1: tdatabutton;
+   tdatabutton2: tdatabutton;
+   trichbutton2: trichbutton;
+   adisp: tintegerdisp;
+   bdisp: tintegerdisp;
+   tbutton1: tbutton;
+   procedure exe(const sender: TObject);
+   procedure aset(const sender: TObject; var avalue: Integer;
+                   var accept: Boolean);
+   procedure bset(const sender: TObject; var avalue: Integer;
+                   var accept: Boolean);
+ end;
+var
+ mainfo: tmainfo;
+implementation
+uses
+ main_mfm;
+ 
+procedure tmainfo.exe(const sender: TObject);
+begin
+ msegui.beep;
+end;
+
+procedure tmainfo.aset(const sender: TObject; var avalue: Integer;
+               var accept: Boolean);
+begin
+ adisp.value:= avalue;
+end;
+
+procedure tmainfo.bset(const sender: TObject; var avalue: Integer;
+               var accept: Boolean);
+begin
+ bdisp.value:= avalue;
+end;
+
+end.
